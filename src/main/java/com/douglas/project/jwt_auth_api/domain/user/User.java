983d6afb85dd043e404e202cab10e8f78e1942b9
Douglas Menezes <douglasgm@MacBook-Air-de-Douglas.local>
2025-04-12 13:00:51 -0300

    add class User and config

---
+++ src/main/java/com/douglas/project/jwt_auth_api/domain/user/User.java
@@ -0,0 +1,113 @@
+package com.douglas.project.jwt_auth_api.domain.user;
+
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "tb_users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.UUID)
+    private UUID id;
+    private String login;
+    private String password;
+    private String role;
+
+    public User() {};
+
+    public User(String password, String role, String login, UUID id) {
+        this.password = password;
+        this.role = role;
+        this.login = login;
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+    // UserDetails config //
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+}
