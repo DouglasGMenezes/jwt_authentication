3e780bf549d067ce90681382daee6fe3c53392fd
Douglas Menezes <douglasgm@MacBook-Air-de-Douglas.local>
2025-04-15 21:44:32 -0300

    add class AuthService

---
+++ src/main/java/com/douglas/project/jwt_auth_api/service/AuthService.java
@@ -0,0 +1,22 @@
+package com.douglas.project.jwt_auth_api.service;
+
+import com.douglas.project.jwt_auth_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+
+
+}
